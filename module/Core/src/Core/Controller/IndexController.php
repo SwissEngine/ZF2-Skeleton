606885ef5a9f99e21dceac0076d594ad5c5df5bf
SwissEngine <thomas.cantonnet+swissengine@gmail.com>
2015-03-04 10:04:27 +0100

    Add docblocks

--- module/Core/src/Core/Controller/IndexController.php
+++ module/Core/src/Core/Controller/IndexController.php
@@ -4,8 +4,15 @@ namespace Core\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
+/**
+ * Class IndexController
+ * @package Core\Controller
+ */
 class IndexController extends AbstractActionController
 {
+    /**
+     * @return ViewModel
+     */
     public function indexAction()
     {
         return new ViewModel();
