be7abb92d7dc995902daade9c7ebbf5ece09fd1b
SwissEngine <thomas.cantonnet+swissengine@gmail.com>
2015-03-04 10:05:00 +0100

    Add

--- module/Core/src/Core/Factory/Controller/IndexControllerFactory.php
+++ module/Core/src/Core/Factory/Controller/IndexControllerFactory.php
@@ -5,8 +5,16 @@ use Core\Controller;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
+/**
+ * Class IndexControllerFactory
+ * @package Core\Factory\Controller
+ */
 class IndexControllerFactory implements FactoryInterface
 {
+    /**
+     * @param ServiceLocatorInterface $serviceLocatorInterface
+     * @return Controller\IndexController
+     */
     public function createService(ServiceLocatorInterface $serviceLocatorInterface)
     {
         return new Controller\IndexController();
