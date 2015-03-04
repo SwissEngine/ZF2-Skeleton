c35782573ba67ed7c01dd9fffb1a32ee4f445783
SwissEngine <thomas.cantonnet+swissengine@gmail.com>
2015-03-04 10:20:56 +0100

    More dockblocks

--- module/Core/Module.php
+++ module/Core/Module.php
@@ -4,8 +4,15 @@ namespace Core;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
+/**
+ * Class Module
+ * @package Core
+ */
 class Module
 {
+    /**
+     * @param MvcEvent $e
+     */
     public function onBootstrap(MvcEvent $e)
     {
         $eventManager        = $e->getApplication()->getEventManager();
@@ -13,11 +20,17 @@ class Module
         $moduleRouteListener->attach($eventManager);
     }
 
+    /**
+     * @return mixed
+     */
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
 
+    /**
+     * @return array
+     */
     public function getAutoloaderConfig()
     {
         return [
